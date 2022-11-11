0fb184ba3b6af6391ae1c7e8462d3fa66993f678
Imhante <72601726+Imhante@users.noreply.github.com>
2022-11-11 04:04:01 +0100

    Updated

---
+++ models/Blog.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const readingTime = require('reading-time')
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      default: "Untitled Post",
+      minlength: 3,
+    },
+    body: {
+      type: String,
+      required: true,
+      minlength: 10,
+    },
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+  
+);
+
+module.exports = new mongoose.model("Post", PostSchema);
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Blog API
+
+A blog API built with Node js
+
+## Features
+- ### Authentication
+    - Sign Up
+    - Login 
+    - Logout
+    - Stateful Jwt Authentication
+- ### Basic CRUD
+    - Create Posts
+    - Get Posts
+    - Update Posts
+
+- ## Testing
+    - All endpoints are tested
+
+## Major Dependencies
+- Mongoose - Interfacing with mongodb 
+- Express js - The core of the API
+- bcrypt - Password hashing
+- jwt - generating and verifying tokens
+- jest - Testing
+
+See package.json for full list of Dependencies
